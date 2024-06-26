ff057fee8e7cd8886800271d7c6eb54878ea209f
vivianwang <vvccwang@gmail.com>
2024-06-26 11:43:57 -0400

    Initial Commit

---
+++ The Culinary Compass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Culinary Compass
+//
+//  Created by Vivian Wang on 2024-06-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Culinary Compass/The_Culinary_CompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Culinary_CompassApp.swift
+//  The Culinary Compass
+//
+//  Created by Vivian Wang on 2024-06-26.
+//
+
+import SwiftUI
+
+@main
+struct The_Culinary_CompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
